cd9dfb5cb7a5834ddb3f7529a0f970ef9bed7d25
Bekzat Bakirov <bekzat.bakirov.alt@gmail.com>
2022-06-03 12:27:56 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+1.
+
+const strToNumber = (str) => {
+    str = Number(str);
+    return str;
+  };
+  
+  console.log(strToNumber("10"));
+
+  
+
+
+//   2.
+  
+//   const isEvenNum = (number) => {
+//     if (number % 2 === 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   };
+//   console.log(isEvenNum(10));
+//   console.log(isEvenNum(3));
+  
+//   3.
+  
+//   const hasElement = (elem, array) => {
+//     let result = array.includes(elem);
+//     return result;
+//   };
+//   let arr = ["Mike", "Zendaya", "Hower"];
+//   console.log(hasElement("Zendaya", arr));
+//   console.log(hasElement("Kloze", arr));
+//   console.log(hasElement("Mike", arr));
